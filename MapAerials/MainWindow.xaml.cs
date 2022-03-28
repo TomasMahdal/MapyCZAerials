cb192f1d7a0b7791413ae02d7d8bc765e4902a8d
Tomáš M <tom.mahdal2002@gmail.com>
2022-03-28 17:22:29 +0200

    basic design of form

--- MapAerials/MainWindow.xaml.cs
+++ MapAerials/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +25,15 @@ namespace MapAerials
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// open hyperlink in user's default web browser
+        /// </summary>
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+
     }
 }
