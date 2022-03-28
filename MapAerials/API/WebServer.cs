d8210bfa3708edff18c63bb868be606f72807a02
Tomáš M <tom.mahdal2002@gmail.com>
2022-03-28 18:55:15 +0200

    WebServer is responding

--- MapAerials/API/WebServer.cs
+++ MapAerials/API/WebServer.cs
@@ -11,36 +11,38 @@ namespace MapAerials.API
 {
     /// <summary>
     /// local WebServer for transfering images into simulator
-    /// WebServer inspired by https://gist.github.com/define-private-public/d05bc52dd0bed1c4699d49e2737e80e7
     /// </summary>
     class WebServer
     {
         public string Url { get; private set; }
         public bool ServerIsRunning { get; private set; }
 
-        private HttpListener httpListener;
+        private TcpListener webListener;
+        private Thread webserverCore;
         private int port = 5050;
-        private string urlStructure = "http://localhost:{0}/";
+        private IPAddress currentIP;
+        private string urlStructure = "http://{0}:{1}/";
 
         /// <summary>
         /// init and start WebServer
         /// </summary>
         public void Start()
         {
+            // get IP
+            currentIP = GetLocalIP();
+
             // create URL
-            Url = string.Format(urlStructure, port);
-
-            // attach HttpListener to URL and start listening
-            httpListener = new HttpListener();
-            httpListener.Prefixes.Add(Url);
-
-            httpListener.Start();
+            Url = string.Format(urlStructure, currentIP, port);
             Console.WriteLine(Url);
+
+            // start listening
+            webListener = new TcpListener(currentIP, port);
+            webListener.Start();
             ServerIsRunning = true;
 
-            // Handle requests
-            Task listenTask = HandleRequest();
-            listenTask.GetAwaiter().GetResult();
+            // run server core thread
+            webserverCore = new Thread(new ThreadStart(WebServerCore));
+            webserverCore.Start();
         }
 
         /// <summary>
@@ -48,37 +50,76 @@ namespace MapAerials.API
         /// </summary>
         public void Stop()
         {
-            // stop server
-            ServerIsRunning = false;
-
-            // stop listener
-            httpListener.Close();
+           
         }
 
-        public async Task HandleRequest()
+        public void WebServerCore()
         {
-            // loop until is WebServer stopped
             while (ServerIsRunning)
             {
-                // get info about connection
-                HttpListenerContext ctx = await httpListener.GetContextAsync();
+                // accept connection
+                Socket socket = webListener.AcceptSocket();
 
-                // get request and response
-                HttpListenerRequest req = ctx.Request;
-                HttpListenerResponse resp = ctx.Response;
+                if (socket.Connected)
+                {
+                    // get data from client
+                    Byte[] bReceive = new Byte[1024];
 
-                Console.WriteLine(req.Url.ToString());
+                    // convert data from client to string
+                    string sBuffer = Encoding.UTF8.GetString(bReceive);
 
-                // Write the response info
-                byte[] data = Encoding.UTF8.GetBytes("Test připojení");
-                resp.ContentType = "text/html";
-                resp.ContentEncoding = Encoding.UTF8;
-                resp.ContentLength64 = data.LongLength;
+                    Console.WriteLine(sBuffer);
 
-                // Write out to the response stream (asynchronously), then close it
-                await resp.OutputStream.WriteAsync(data, 0, data.Length);
-                resp.Close();
+                    SendToBrowser("test", socket);
+                }
             }
+        }
+
+        /// <summary>
+        /// Send data to browser
+        /// </summary>
+        /// <param name="returnData">HTML string</param>
+        /// <param name="socketObj">socket object</param>
+        public void SendToBrowser(string returnData, Socket socketObj)
+        {
+            Byte[] dataForSend = Encoding.UTF8.GetBytes(returnData);
+            SendHeader(dataForSend.Length, "200", socketObj);
+            if (socketObj.Connected)
+            {
+                socketObj.Send(dataForSend);
+            }
+        }
+
+        /// <summary>
+        /// Send response header to browser
+        /// </summary>
+        private void SendHeader(int lenghtOfBytes, string statusCode, Socket socketObj, string contentType = "text/html")
+        {
+            string sBuffer = "";
+
+            sBuffer = sBuffer + "HTTP/1.1 " + statusCode + "\r\n";
+            sBuffer = sBuffer + "Server: LOTUSWebServer\r\n";
+            sBuffer = sBuffer + "Content-Type: " + contentType + "\r\n";
+            sBuffer = sBuffer + "Connection: close\r\n";
+            sBuffer = sBuffer + "Accept-Ranges: bytes\r\n";
+            sBuffer = sBuffer + "Content-Length: " + lenghtOfBytes + "\r\n\r\n";
+
+            if (socketObj.Connected)
+            {
+                socketObj.Send(Encoding.ASCII.GetBytes(sBuffer));
+            }
+        }
+
+        private static IPAddress GetLocalIP()
+        {
+            foreach (IPAddress ip in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip;
+                }
+            }
+            throw new Exception("No network adapter with IPv4 address found!");
         }
     }
 }
